dcf2e08d774e8c7f2f0867e13a2121abf2ba0e1a
Beon de Nood <beon@bigdune.net>
2025-11-21 10:54:20 -0500

    fix: bump package version to 2.1.3 but keep core version at 1.0.2

--- src/capiscio/manager.py
+++ src/capiscio/manager.py
@@ -17,7 +17,7 @@ console = Console()
 logger = logging.getLogger(__name__)
 
 # Configuration
-CORE_VERSION = "1.0.2"  # Matches the python package version
+CORE_VERSION = "1.0.2"  # The version of the core binary to download
 GITHUB_REPO = "capiscio/capiscio-core"
 BINARY_NAME = "capiscio"
 
